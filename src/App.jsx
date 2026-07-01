a24c6ff7699d07e7444215db64a57badde9f9caf
Batuhan Batur <batu@batuhanbatur.com>
2026-07-01 18:49:05 +0300

    feat: vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { OrderProvider } from './context/OrderContext'
 import { AccessibilityProvider } from './context/AccessibilityContext'
+import { Analytics } from "@vercel/analytics/react"
 import Landing from './pages/Landing'
 import OrderFlow from './pages/order/OrderFlow'
 
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/order/ai" element={<OrderFlow />} />
         </Routes>
+        <Analytics />
       </OrderProvider>
     </AccessibilityProvider>
   )
